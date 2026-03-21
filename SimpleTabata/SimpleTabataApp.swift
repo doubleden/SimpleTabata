f2bcf449baaa73599ceb1ae616f9191413c8b591
Denis Denisov <denis.denisov8701@gmail.com>
2026-03-21 14:41:16 +0100

    Initial Commit

---
+++ SimpleTabata/SimpleTabataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTabataApp.swift
+//  SimpleTabata
+//
+//  Created by Denis Denisov on 21/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTabataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
